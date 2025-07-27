efd2a1fee8436f94274bbcbbf4a06967edadad85
mehedihasan53 <mehedihasan.codes3@gmail.com>
2025-07-27 19:58:20 +0600

    task_3

---
+++ task-3.js
@@ -0,0 +1,15 @@
+
+// ### Task-3
+
+// You task is to divide the given number by 5 and show the remainder as the output.
+
+// **Input:**
+// </br>
+// The first line of the input contains the number.
+
+
+
+var number=119;
+var number2=5;
+var result=number%number2;
+console.log(result); 
