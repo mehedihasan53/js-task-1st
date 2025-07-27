7e8bc8b4d02069b4de711e8661c5677137092a04
mehedihasan53 <mehedihasan.codes3@gmail.com>
2025-07-27 20:26:17 +0600

    task_4

---
+++ task-4.js
@@ -0,0 +1,8 @@
+// What will be the result of the following codes:
+// var a = isNaN('11'); 
+
+var a = isNaN('11'); 
+console.log("The value of a is "+a);
+
+var b = isNaN(2-10);
+console.log("The value of b is "+b);
