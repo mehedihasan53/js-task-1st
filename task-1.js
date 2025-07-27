2aa3f968075a8713ecd349edc2176e5ee15c0522
mehedihasan53 <mehedihasan.codes3@gmail.com>
2025-07-27 19:52:51 +0600

    task_1

--- task-1.js
+++ task-1.js
@@ -1,3 +1,17 @@
+// ### Task-1
+
+// You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+
+// **Input:**
+// </br>
+// The first line of the input is the taka you have.
+// The second line is the cost of 1 kg of oranges and 1 kg of apples.
+
+// **Output:**
+// </br>
+// Print the result.
+
+
 var totalTaka = 5000;
 var applePrice=300;
 var orangePrice=400;
